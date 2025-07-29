650d28464321adb0072d6a1dfcf8d4c79eb4d183
Omar Mammadov <mammadovomar2008@gmail.com>
2025-07-29 19:26:48 +0400

    Update defines.h

--- defines.h
+++ defines.h
@@ -97,47 +97,5 @@ STATIC_ASSERT(sizeof(f64) == 8, "Expected f64 to be 8 bytes.");
 #define false 0
 #define False 0
 
-/*
-// Platform detection
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
-#define PLATFORM_WINDOWS 1
-#ifndef _WIN64
-#error "64-bit is required on Windows!"
-#endif
-#elif defined(__linux__) || defined(__gnu_linux__)
-// Linux OS
-#define PLATFORM_LINUX 1
-#if defined(__ANDROID__)
-#define PLATFORM_ANDROID 1
-#endif
-#elif defined(__unix__)
-// Catch anything not caught by the above.
-#define PLATFORM_UNIX 1
-#elif defined(_POSIX_VERSION)
-// Posix
-#define PLATFORM_POSIX 1
-#elif __APPLE__
-// Apple platforms
-#define PLATFORM_APPLE 1
-#include <TargetConditionals.h>
-#if TARGET_IPHONE_SIMULATOR
-// iOS Simulator
-#define PLATFORM_IOS 1
-#define PLATFORM_IOS_SIMULATOR 1
-#elif TARGET_OS_IPHONE
-#define PLATFORM_IOS 1
-// iOS device
-#elif TARGET_OS_MAC
-// HACK: Should probably be in the Vulkan Renderer lib, not here.
-#define VK_USE_PLATFORM_MACOS_MVK
-// Other kinds of Mac OS
-#else
-#error "Unknown Apple platform"
-#endif
-#else
-#error "Unknown platform!"
-#endif
-
-*/
 #endif // DEFINES_H_INCLUDED
 
